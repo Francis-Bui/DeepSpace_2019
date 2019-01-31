870425a087088c79f1b9656b86a59b170852accb
frc7664 <frc7664@gmail.com>
2019-01-31 14:36:38 -0500

    OperateIntake, BallIntake. Push still contains Operate Intake collision error.

---
+++ src/main/java/frc/robot/subsystems/BallIntake.java
@@ -0,0 +1,52 @@
+package frc.robot.subsystems;
+
+import frc.robot.RobotMap;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.commands.OperateIntake;
+
+
+/**
+ * Subsystem class for the ball intake
+ */
+public class BallIntake extends Subsystem {
+
+    VictorSP BALL_INTAKE_MOTOR;
+
+    
+	private static BallIntake instance = null;
+    
+	public BallIntake() {
+        BALL_INTAKE_MOTOR = new VictorSP(RobotMap.BALL_INTAKE_MOTOR);
+    }
+
+	public static BallIntake getInstance(){
+		if(instance == null)
+			instance = new BallIntake();
+		
+		return instance;
+	}
+
+	/**
+	 * 
+	  @param speed 1.0 to -1.0, positive values bring balls into the robot.
+	 */
+
+    public void runintake(double speed){
+        BALL_INTAKE_MOTOR.set(speed);
+        
+    }
+	
+
+    @Override
+    public void initDefaultCommand() {
+       setDefaultCommand(new OperateIntake());
+    }
+    public void button() {
+        //sets the button int
+
+
+
+
+    }
+}
